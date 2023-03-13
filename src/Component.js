f96778793676f341f606d39250cc93292ed96f17
Ivan Yaremko <yaremich98@gmail.com>
2023-03-13 09:02:57 +0200

    (56) Webpack

---
+++ src/Component.js
@@ -0,0 +1,11 @@
+class Component {
+   constructor(prop) {
+    this.prop = prop;
+   } 
+   render() {
+    console.log('render');
+   }
+}
+
+module.exports = Component;
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('./style.css')
+const Component = require('./Component');
+
+console.log('test');
+const resultOfSumTwoNumber = 5+3;
+console.log(resultOfSumTwoNumber);
+const comp = new Component(12);
